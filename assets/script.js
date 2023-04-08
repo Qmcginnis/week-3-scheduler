5b073ac388ebf6e45e6a8c54ae656314bd0f9b7c
Qmcginnis <qmcginnis500@gmail.com>
2023-04-08 18:10:28 -0400

    Adding global variables on JS

--- assets/script.js
+++ assets/script.js
@@ -25,6 +25,19 @@ function updateTime() {
       }
   }
 }
+
+let saveBttn = $(".save-icon");
+let containerEl = $(".container");
+let schedule9am = $("#9AM");
+let schedule10am = $("#10AM");
+let schedule11am = $("#11AM");
+let schedule12pm = $("#12PM");
+let schedule1pm = $("#1PM");
+let schedule2pm = $("#2PM");
+let schedule3pm = $("#3PM");
+let schedule4pm = $("#4PM");
+let schedule5pm = $("#5PM");
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
